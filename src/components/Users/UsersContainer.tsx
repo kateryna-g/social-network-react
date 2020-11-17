65c13d07cb6d35861f8b700990ed8979cda66abb
kateryna-g <kat.hryhorieva@gmail.com>
2020-11-17 15:16:07 +0200

    2.0.9.added types for Actions Creators, Generic, infer

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-import {follow, requestUsers, setCurrentPage, toggleFollowingProgress, unfollow} from "../../redux/users-reducer";
+import {follow, requestUsers, unfollow} from "../../redux/users-reducer";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
 import {compose} from "redux";
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -23,6 +23,10 @@ type RootReducerType = typeof rootReducer // ( globalState : AppStateType ) => A
 export type AppStateType = ReturnType<RootReducerType>
 
 
+type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
+export type InferActionsTypes<T extends { [key: string]: (...args: any[])=>any }> = ReturnType<PropertiesTypes<T>>
+
+
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
